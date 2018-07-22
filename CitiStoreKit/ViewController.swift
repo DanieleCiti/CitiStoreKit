b25604576eb774b0b45f725604f5441bf821b917
Daniele Citi <31421274+DanieleCiti@users.noreply.github.com>
2018-07-23 00:01:30 +0200

    Update ViewController.swift

--- CitiStoreKit/ViewController.swift
+++ CitiStoreKit/ViewController.swift
@@ -8,18 +8,40 @@
 
 import UIKit
 
+// In order to make the CSKService class works, remember to install the SwiftyStoreKit framework via CocoaPod into your project.
+
 class ViewController: UIViewController {
+    
+    // 1) Decleare a variable of type CSKService
+    var inAppService: CSKService!
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+  
+        // 2) Set the delegate and the shared secret properties.
+        inAppService.delegate = self
+        inAppService.sharedSecret = "Your shared Secret. You can get in on iTunes Connect"
+    }
+    
+    // 3) Just call the functions you need.
+    func exampleFuncPurchase()
+    {
+        inAppService.purchaseProduct(with: "InAppID", type: .simple, successSegueID: "yourID", errorSegueID: "yourID")
+    }
+    
+    func exampleVerifyPurchase()
+    {
+        inAppService.verifyPurchase(with: "inAppID", type: .autoRenewable, segueIDPurchased: "yourID", segueIDFailed: "YourID")
+    }
+    
+    func exampleRestorePurchase()
+    {
+        inAppService.restorePurchase(segueIDFailed: "yourID", segueIDSuccess: "yourID", segueIDNothingToRestore: "yourID")
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
-
+    // Finish. Easy, isn't it? :)
+    
 }
+
+
 
