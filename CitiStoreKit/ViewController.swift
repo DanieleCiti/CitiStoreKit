32a6b8d705cbb776efae926c7e75ec4813ea4ae5
Daniele Citi <citidaniele17@gmail.com>
2018-07-22 19:58:26 +0200

    Initial Commit

---
+++ CitiStoreKit/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  CitiStoreKit
+//
+//  Created by Daniele Citi on 22/07/18.
+//  Copyright © 2018 Daniele Citi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
